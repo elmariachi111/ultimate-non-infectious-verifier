08fd7dacec10a1492de09e9552b2a69262885358
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2021-02-12 22:43:13 +0100

    ui fixes

--- packages/immu-verifier/src/pages/RequestPresentationPage.tsx
+++ packages/immu-verifier/src/pages/RequestPresentationPage.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import crypto from 'crypto';
 import bs58 from 'bs58';
 import QRCode from 'qrcode';
-import { Alert, AlertIcon, Box, Button, Heading, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useClipboard } from '@chakra-ui/react';
+import { Alert, AlertIcon, Box, Button, Code, Heading, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useClipboard } from '@chakra-ui/react';
 
 interface PresentationResponseAttrs {
   presentationResponse: string;
@@ -79,11 +79,15 @@ const RequestPresentationPage: React.FC = () => {
     <Box mt={6}>{presentationRequestJwt &&
       <Box>
         <Heading>present your credentials</Heading>
-        <Text>We're accepting <ul>
+        <Box>
+          <Text>We're accepting </Text>
+          <Box>
           {credentialVerifier.supportedStrategies.map(strategy => (
-            <li>{strategy}</li>
+            <Code key={`strategy-${strategy}`}>{strategy}</Code>
           ))}
-        </ul> at the moment</Text>
+          </Box>
+          <Text>at the moment</Text>
+        </Box>
         <img src={presentationJwtQrCode} alt="qr code" onClick={onCopy} />
         <code >{presentationRequestJwt}</code>
       </Box>
