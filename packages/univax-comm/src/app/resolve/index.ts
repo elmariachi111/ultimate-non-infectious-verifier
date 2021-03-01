862bae57197d7275486bf26290e2a8197f454fc4
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2021-03-01 13:46:59 +0100

    (comm) uses new resolver builder

--- packages/univax-comm/src/app/resolve/index.ts
+++ packages/univax-comm/src/app/resolve/index.ts
@@ -1,32 +1,44 @@
-import { Resolver } from '@univax/core';
+import { ResolverBuilder, Sidetree, EthProviderConfig } from '@univax/core';
 import { Router } from 'express';
 import { RESOLVER_ENDPOINT } from '../../constants/endpoint';
+const { NODE_ENV, ETHEREUM_NODE, SIDETREE, REGISTRY, IPFS_API, MONGO_CONNECTION, INFURA_ID } = process.env;
+
+let ethNetworks: EthProviderConfig[] = [];
+
+if (INFURA_ID) {
+  ethNetworks = [...ethNetworks, ...ResolverBuilder.ethProviderConfig(INFURA_ID)];
+}
+
+if (NODE_ENV == 'development') {
+  ethNetworks = [
+    ...ethNetworks,
+    {
+      name: 'development',
+      rpcUrl: ETHEREUM_NODE,
+      registry: REGISTRY
+    }
+  ];
+}
+
+const builder = ResolverBuilder().addKeyResolver().addEthResolver(ethNetworks);
+
+if (SIDETREE && ETHEREUM_NODE && IPFS_API && MONGO_CONNECTION) {
+  builder.addSideTreeResolver(
+    Sidetree.SidetreeElemMethod({
+      eth: {
+        node: ETHEREUM_NODE,
+        sideTreeContractAddress: SIDETREE
+      },
+      ipfsNode: IPFS_API,
+      mongoConnection: MONGO_CONNECTION,
+      dbName: 'element-test'
+    })
+  );
+}
+
+const resolver = builder.build();
 
 export const router: Router = Router();
-
-export const DB_FILENAME = './records.db';
-
-const ethrDidConfig = [...Resolver.ethProviderConfig(process.env.INFURA_ID!)];
-if (process.env.NODE_ENV === 'development') {
-  ethrDidConfig.push({
-    name: 'development',
-    rpcUrl: process.env.ETHEREUM_NODE!,
-    registry: process.env.REGISTRY!
-  });
-}
-
-const resolver = new Resolver(ethrDidConfig);
-if (process.env.SIDETREE) {
-  resolver.initializeSidetreeResolver({
-    eth: {
-      node: process.env.ETHEREUM_NODE!,
-      sideTreeContractAddress: process.env.SIDETREE!
-    },
-    ipfsNode: process.env.IPFS_API!,
-    mongoConnection: process.env.MONGO_CONNECTION!,
-    dbName: 'element-test'
-  });
-}
 
 router.post(RESOLVER_ENDPOINT, async (req, res) => {
   const did = req.body.did;
