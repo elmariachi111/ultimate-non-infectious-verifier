a04d407e12b239329ace2c624b8ff053464af034
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2021-03-01 22:46:23 +0100

    (core) new resolver tests

--- packages/univax-core/src/__tests__/Resolver.test.ts
+++ packages/univax-core/src/__tests__/Resolver.test.ts
@@ -2,9 +2,57 @@ import { Ed25519Signing, ResolverBuilder } from '../';
 import web3 from './common/web3Provider';
 import newRegistry from './common/newRegistry';
 import { Resolvable } from 'did-jwt';
+import nock from 'nock';
+import { DIDDocument, ParsedDID, Resolver } from 'did-resolver';
 
 describe('Resolver', () => {
   let resolver: Resolvable;
+  describe('fallbackResolver', () => {
+    beforeAll(() => {
+      const scope = nock('http://resolver').post('/resolve', { did: 'did:test:x1234' }).reply(200, {
+        id: 'did:test:x1234'
+      });
+    });
+
+    it('can fallback to an async remote resolver', async () => {
+      resolver = ResolverBuilder().addRemoteFallbackResolver('http://resolver/resolve').build();
+      const someDoc = await resolver.resolve('did:test:x1234');
+      expect(someDoc).toEqual({ id: 'did:test:x1234' });
+    });
+  });
+
+  describe('customResolver', () => {
+    it('can register a custom resolver', async () => {
+      const customRegistry = {
+        test: async (did: string, parsed: ParsedDID, resolver: Resolver): Promise<null | DIDDocument> => {
+          return Promise.resolve({
+            '@context': 'https://w3id.org/did/v1',
+            id: did,
+            publicKey: []
+          });
+        }
+      };
+      resolver = ResolverBuilder().addCustomResolver(customRegistry).build();
+      const resolved = await resolver.resolve('did:test:xCanResolve');
+      expect(resolved.id).toEqual('did:test:xCanResolve');
+    });
+
+    it('can register a custom resolver asynchronously', async () => {
+      const customRegistry = Promise.resolve({
+        test: async (did: string, parsed: ParsedDID, resolver: Resolver): Promise<null | DIDDocument> => {
+          return Promise.resolve({
+            '@context': 'https://w3id.org/did/v1',
+            id: did,
+            publicKey: []
+          });
+        }
+      });
+      resolver = ResolverBuilder().addCustomResolver(customRegistry).build();
+      const resolved = await resolver.resolve('did:test:xCanResolve');
+      expect(resolved.id).toEqual('did:test:xCanResolve');
+    });
+  });
+
   describe('ethrResolver', () => {
     beforeAll(async () => {
       const registry = await newRegistry(web3);
